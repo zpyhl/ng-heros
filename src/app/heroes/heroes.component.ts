0668fa337e6f76b0014856621b2a678f78f6c0c7
hanlei <hl@qq.com>
2019-05-28 15:18:58 +0800

    push hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
+
+// hero = 'Windstorm';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,15 +10,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.sass']
 })
 export class HeroesComponent implements OnInit {
-  // hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+  
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
