a1f90fcd80ff4e3e8fcdc5e6c21225b110a9587e
hanlei <hl@qq.com>
2019-05-29 12:16:07 +0800

    service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 // hero = 'Windstorm';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes : Hero[] ;
   selectedHero: Hero;
 
-  constructor() {}
+  constructor(private heroService : HeroService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  };
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes =>{
+        this.heroes = heroes;
+    });
   }
+
 }
